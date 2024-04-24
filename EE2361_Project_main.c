bd2f3e51a8f4ffa750bbb941bab84858dbaf3524
E-vehicle <153661936+E-vehicle@users.noreply.github.com>
2024-04-24 17:05:56 -0500

    Update EE2361_Project_main.c

--- EE2361_Project_main.c
+++ EE2361_Project_main.c
@@ -20,7 +20,7 @@ void __attribute__((interrupt, auto_psv)) _T2Interrupt(void) {
     sec++;
 }
 
-volatile unsigned long int buffer[3];
+volatile unsigned long int buffer[3] = {0,0,0};
 volatile unsigned int bufferPlace = 0;
 
 volatile unsigned long int eventTime = 0;
@@ -29,67 +29,33 @@ volatile unsigned long int tripleClickTime = 0;
 volatile unsigned int interruptVar = 0;
 volatile unsigned int conditionMetCount = 0;
 volatile unsigned long int lastRecordedEventTime = 0;
+volatile unsigned int numKnocks = 0;
+volatile unsigned int enable = 0;
 
-void __attribute__ ((__interrupt__)) _AD1C1Interrupt(void) {
-    _AD1IF = 0;
+void __attribute__ ((interrupt, auto_psv)) _ADC1Interrupt(void) {
     
     int adValue = ADC1BUF0;
-    eventTime = IC1BUF + (sec*62500L);
+    
     
     // Check if the ADC value is outside the bounds
-    if (adValue < -100 || adValue > 100) {
-        // Increment the count of times the condition is met
-        conditionMetCount++;
-        
-        // Record the time of the event
-        lastRecordedEventTime = eventTime;
-        
-        // Check if the condition has been met three times within the specified time frame
-        if (conditionMetCount >= 3 && (eventTime - tripleClickTime) > 300000) {
-            // Perform actions when the condition is met three times
-            setServo(3000);
-            tripleClickTime = eventTime;
-            LATBbits.LATB13 = 0;
-            LATBbits.LATB15 = 1;
-            delay(2000);
-            LATBbits.LATB15 = 0;
-            LATBbits.LATB13 = 1;
-            buffer[0] = 0;
-            buffer[1] = 0;
-            buffer[2] = 0;
-            // Reset the count of times the condition is met
-            conditionMetCount = 0;
-            return;
-        }
-    } else {
-        // Reset the count of times the condition is met if the value is within bounds
-        conditionMetCount = 0;
+    if (adValue >= 50) {
+
+        LATBbits.LATB14 = 1;
+		delay(500);
+        LATBbits.LATB14 = 0;
+
     }
-    
-    // Your existing code for handling the case when the value is within bounds
-    if ((eventTime - lastEventTime) > 50000) {
-        buffer[bufferPlace] = eventTime;
-        bufferPlace = (bufferPlace + 1) % 3;
-        if (((eventTime - buffer[bufferPlace]) < 300000)) {
-            setServo(3000);
-            tripleClickTime = eventTime;
-            LATBbits.LATB13 = 0;
-            LATBbits.LATB15 = 1;
-            delay(2000);
-            LATBbits.LATB15 = 0;
-            LATBbits.LATB13 = 1;
-            buffer[0] = 0;
-            buffer[1] = 0;
-            buffer[2] = 0;
-            return;
-        }
+    if (adValue > 0 && adValue < 50) {
+
+        LATBbits.LATB15 = 1;
+		delay(500);
+        LATBbits.LATB15 = 0;
+
+
     }
-    
-    LATBbits.LATB14 = 1;
-    delay(100);
-    LATBbits.LATB14 = 0;
-    lastEventTime = eventTime;
+    _AD1IF = 0;
 }
+   
 
 void setup() {
 	
@@ -99,17 +65,8 @@ void setup() {
     TRISBbits.TRISB14 = 0;      // Yellow LED output
     TRISBbits.TRISB13 = 0;      // Red LED output
     TRISBbits.TRISB6 = 0;       // Servo motor output
-    AD1CON1bits.SSRC = 0b010;
     
-    AD1CON2 = 0;
-    AD1CON3bits.SAMC = 0b101;
-    AD1CHS = 0;
-    AD1CSSL = 0;
-    AD1PCFGbits.PCFG0 = 0;
-    TRISAbits.TRISA0 = 1;
-    IFS0bits.AD1IF = 0;
-    IEC0bits.AD1IE = 1;
-    AD1CON1bits.ADON = 1; //turn it on
+    
     
     // TRISBbits.TRISB7 = 1;       // Button input IF WE WANT TO ADD BUTTON
     TRISBbits.TRISB8 = 1;       // Piezo input
@@ -120,13 +77,7 @@ void setup() {
 }
 
 void initPiezo(void) {
-    // Configure Timer 2 (500ns / count, 25ms max).
-    // note that resolution = 500ns = 8 x 62.5ns, max period = 25ms = Tcy * 8 * 50,000
-    
-    __builtin_write_OSCCONL(OSCCON & 0xbf);
-    RPINR7bits.IC1R = 8;
-    __builtin_write_OSCCONL(OSCCON | 0x40);
-    
+ 
     T2CONbits.TON = 0;
     T2CONbits.TCKPS = 0b11;
     T2CONbits.TCS = 0b0;
@@ -135,19 +86,31 @@ void initPiezo(void) {
     // Initialize to zero (also best practice)
     PR2 = 0xF424; // Set period to one second
     T2CONbits.TON = 1; // Start 16-bit Timer2
+
+    TRISAbits.TRISA0 = 1;
     
-    // Initialize the Input Capture Module
-    IC1CONbits.ICTMR = 1; // Select Timer2 as the IC1 Time base
-    IC1CONbits.ICI = 0b00; // Interrupt on every capture event
-    IC1CONbits.ICM = 0b011; // Generate capture event on every Rising edge
-    // Enable Capture Interrupt And Timer2
+    AD1PCFGbits.PCFG0 = 0;
     
-    IPC0bits.IC1IP = 1; // Setup IC1 interrupt priority level
-    IFS0bits.IC1IF = 0; // Clear IC1 Interrupt Status Flag
-    IEC0bits.IC1IE = 1; // Enable IC1 interrupt
     
-    IFS0bits.T2IF = 0; // Clear IC1 Interrupt Status Flag
-    IEC0bits.T2IE = 1; // Enable IC1 interrupt
+    AD1CON2bits.VCFG = 0;
+    AD1CON3bits.ADCS = 0b00000011;
+    AD1CON1bits.SSRC = 0b010;
+    AD1CON3bits.SAMC = 0b00011;
+    AD1CON1bits.FORM = 0b00;
+    
+    AD1CON1bits.ASAM = 1;
+    AD1CON2bits.SMPI = 0b0000;
+    AD1CON1bits.ADON = 1; //turn it on 
+    
+    _AD1IF = 0;
+    _AD1IE = 1;
+    
+    TMR3 = 0;
+    T3CON = 0;
+    T3CONbits.TCKPS = 0b10;
+    PR3 = 15624;
+    T3CONbits.TON = 1;
+    
 }
 
 void delay(unsigned int ms) {
@@ -162,16 +125,10 @@ void delay(unsigned int ms) {
 int main(void) {
 	setup();
 	setServo(3600);
+    LATBbits.LATB13 = 1;
+    int newestVal = 0;
 	while (1) {
-        if ((TMR2 + (sec*62500L) - tripleClickTime) > (PR2*3)) {
-            setServo(3600);
-			LATBbits.LATB13 = 1;
-			delay(1000);
-			LATBbits.LATB13 = 0;
-        }
-        else {
-            setServo(3000);
-        }
+
     }
-	return 0;
+    return 0;
 }
